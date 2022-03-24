b3f4d703a3ba07277b35668b485ecae56ec1251e
PaidForWinRAAR <daniel.holmkvist833@gmail.com>
2022-03-24 11:52:12 +0100

    PIDParam, PID

--- src/PID.rs
+++ src/PID.rs
@@ -1,3 +1,5 @@
+use std::sync::RwLock;
+
 pub struct PID{
     p: PIDParam,
     v: f64, 
@@ -7,34 +9,53 @@ pub struct PID{
     D: f64,
     I: f64,
     ad: f64,
-    bd: f64
+    bd: f64,
+    lock: RwLock
 }
 
 impl PID{
-    fn calculateOutput(&mut self, y: f64, yref: f64) -> f64{
+    pub fn new() -> Self {
+        Self{
+            lock: RwLock::new(PIDParam), 
+            I: 0,
+            D: 0,
+            v: 0,
+            e: 0,
+            ynew: 0,
+            yold: 0,
+            ad: 0,
+            bd: 0,
+            p: PIDParam::new(-0.1, 0.0, 0, 1.0, 5.0, 1.0, 0.1, false)
+        }
+    }
+
+    pub fn calculateOutput(&mut self, y: f64, yref: f64) -> f64{
+        let param = lock.read().unwrap();
         self.ynew = y;
         self.e = yref - y;
         self.D = self.ad*self.D - self.bd*(y - self.yold);
-        self.v = self.p.K*(self.p.Beta*yref - y) + self.I + self.D;     
+        self.v = param.K*(param.Beta*yref - y) + self.I + self.D;     
         return self.v;
     }
 
-    fn updateState(&mut self, u: f64){
-        if (self.p.integratorOn) {
+    pub fn updateState(&mut self, u: f64){
+        let param = lock.read().unwrap();
+        if (param.integratorOn) {
             // Forward Euler approximation
-            self.I += (self.p.K * self.p.H / self.p.Ti) * self.e + (self.p.H / self.p.Tr) * (u - self.v);
+            self.I += (param.K * param.H / param.Ti) * self.e + (param.H / param.Tr) * (u - self.v);
         } else {
             self.I = 0;
         }
         self.yold = self.ynew;
     }
 
-    fn getHMillis() -> f64{
+    pub fn getHMillis() -> f64{
         return (self.p.H * 1000);
     }
 
-    fn setParameters() {
-        p = (PIDParameters) newParameters.clone();
+    pub fn setParameters() {
+        let mut w = lock.write.unwrap();
+        *w = newParameters.copy() as PIDParameter;
         if (!p.integratorOn) {
             I = 0;
         }
